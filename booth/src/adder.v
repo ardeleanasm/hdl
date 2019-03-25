6f28d1b0b86b8cecd2ff68814ce841a8a50dbdef
Sebastian Ardelean <37292233+sebastianardelean@users.noreply.github.com>
2019-03-25 08:39:29 +0200

    Update adder.v

--- booth/src/adder.v
+++ booth/src/adder.v
@@ -10,21 +10,23 @@ module adder(
    
    input 	 ctl_add;
    input 	 ctl_sub;
-   
+   input ctl_init;
    input [REG_WIDTH-1] input_a;
    input [REG_WIDTH-1] input_b;
    output reg [REG_WIDTH-1] output_c;
 
    always @({ctl_add,ctl_sub,ctl_init}) begin
       case ({ctl_add,ctl_sub,ctl_init})
-	3'b100: begin
-	   output_c = input_a + input_b;
-	end
-	3'b110: begin
-	   output_c = input_a - input_b;
-	3'b001: begin
-	   output_c = 0;
-	end
+      3'b100: begin
+         output_c = input_a + input_b;
+      end
+      3'b110: begin
+         output_c = input_a - input_b;
+      end
+      3'b001: begin
+         output_c = 0;
+      end
+      endcase
    end // always @(control)
    
 endmodule // adder
