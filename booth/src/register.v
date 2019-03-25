3dbcab67799911f5218d359e71a78935499236f4
ars <ardeleanasm@gmail.com>
2019-03-25 08:40:56 +0200

    Update register.v

--- booth/src/register.v
+++ booth/src/register.v
@@ -5,7 +5,8 @@ module register(
 		dump,
 		shr,
 		shiftout,
-		shiftin
+		shiftin,
+  		lsb
 		);
 
    
@@ -18,7 +19,7 @@ module register(
 		  
    input 		  shiftin;
    output reg 		  shiftout;
-      
+   output lsb;
    output reg [REG_WIDTH-1:0] outbus;
   
 		   
@@ -26,20 +27,19 @@ module register(
 
    always @({load,dump,shr}) begin
       case ({load,dump,shr})
-	3'b100: begin
-	   storage = inbus;
-	   shiftout = 1'b0;
-	end
-	5'b010: begin
-	   outbus = storage;
-	end
-	5'b001: begin
-	   shiftout = storage[0];
-	   storage = {shiftin,storage >>1};
-	end
+      3'b100: begin
+         storage = inbus;
+         shiftout = 1'b0;
+      end
+      5'b010: begin
+         outbus = storage;
+      end
+      5'b001: begin
+         shiftout = storage[0];
+         storage = {shiftin,storage >>1};
+      end
       endcase // case ({load,dump,shl,shr})
       
    end // always @ (control)
-   
+  assign lsb=storage[0];
 endmodule // register
-
